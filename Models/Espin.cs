fabad920224f25032410e3f5982a39debeaa4c1e
Junior Humberto Espin Escobar <junior.espin@udla.edu.ec>
2024-05-02 07:35:15 -0500

    Modelos Junior Espin

---
+++ Models/Espin.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruebaProgreso1Espin.Models
+{
+    public class Espin
+    {
+        [Key]
+        private int DNI();
+
+        private float? Calificacion;
+
+        [Required]
+        private string Nombre;
+
+        private bool Ecuatoriano;
+        private DateTime Nacimiento;
+        [Required, ForeignKey("Carrera ID")]
+
+        private int CarreraID;
+        private Carrera? Carrera;
+        
+    }
+}
